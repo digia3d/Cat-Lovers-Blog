4acb393f1e955ae9125871807315a37470d83fad
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-19 16:33:44 +0200

    Add the references to the author as well for the likes table and for the comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_123217) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_142352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_123217) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,5 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_123217) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
