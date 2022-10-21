b139f101e9ee6c76e8b77ff4a9555b54966182d6
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-21 22:00:24 +0200

    Generate users controler, views and helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  routh 'users#index'
+
+  get 'users' to: 'users#index'
+  get 'users/:id' to: 'users#show'
+  get 'posts/show'
   resources :likes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
