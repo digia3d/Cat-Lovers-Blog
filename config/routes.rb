8aef310e028cbfe8088d5e5f076085b6c2d7c1be
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-21 22:33:56 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  routh 'users#index'
+  root 'users#index'
 
-  get 'users' to: 'users#index'
-  get 'users/:id' to: 'users#show'
-  get 'posts/show'
-  resources :likes
+  get 'users/',  to: 'users#index'
+  get '/users/:id',  to: 'users#show'
+
+  get '/users/:user_id/posts',  to: 'posts#index'
+  get '/users/:user_id/posts/:id',  to: 'posts#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
