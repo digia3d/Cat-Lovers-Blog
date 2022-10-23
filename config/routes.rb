d07b6716057ab08431cd5fc4f60fca915cc94cbd
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-23 18:44:18 +0200

    Add action to controlers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  get 'users/',  to: 'users#index'
+  get '/users',  to: 'users#index'
   get '/users/:id',  to: 'users#show'
 
   get '/users/:user_id/posts',  to: 'posts#index'
