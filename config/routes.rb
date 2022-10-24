d877f4fb2c3e7447ba5412231c5006cbd7d1196a
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-24 09:44:52 +0200

    Fix routes and linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
-  get '/users',  to: 'users#index'
-  get '/users/:id',  to: 'users#show'
-
-  get '/users/:user_id/posts',  to: 'posts#index'
-  get '/users/:user_id/posts/:id',  to: 'posts#show'
-
+  root to: 'users#index'
 end
