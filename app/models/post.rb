54e5d622a2cf13d3b5145453252380ed3e8937a7
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-26 22:17:32 +0200

    Fix sitax errors, postgres conection, and controlers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
-
-  validates :title, presence: true, length: { maximum: 100 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  has_many :comments
+  has_many :likes
 
   after_save :update_posts_counter
 
