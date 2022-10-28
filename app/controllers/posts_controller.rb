c9e3bf1fe2cd045560da96c61f3192638fee71eb
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-28 10:02:01 +0200

    Add methods in likes controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @user = current_user
+    @post.users = @user
+
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new, :status => :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
