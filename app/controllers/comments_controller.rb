86168d87644d6b9cc97c6e019a0f16a6b584e7b2
Aleksandra Ujvari <ujvari65@gmail.com>
2022-10-28 09:35:51 +0200

    Add form for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:text, :post_id)
+    params.require(:comment).permit(:text, :user_id)
   end
 end
