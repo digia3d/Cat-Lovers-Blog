09087d7ce8e23c27e36dc8522a7c90a0996aade9
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-01 09:08:13 +0100

    Add tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, foreign_key: 'post_id'
 
   after_commit :update_comment_counter, on: :create
 
