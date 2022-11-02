bb3e921df90d6e83e2e6f67fbd1217477eadb436
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-02 11:36:40 +0100

    Update post controller by adding like to method show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(posts: [:comments]).find(params[:user_id])
   end
 
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
+    @like = Like.new
   end
 
   def new
