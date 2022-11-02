9c5163acd27e6a2a67f9d53061ac21c3ecf828a9
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-02 14:04:07 +0100

    Add sign out button,and update app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :update_allowed_parameters, if: :devise_controller?
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 
-  protected
+  def route_not_found
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do
-        |u| u.permit(:name, :photo, :bio, :email, :password, :current_password)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 end
 
