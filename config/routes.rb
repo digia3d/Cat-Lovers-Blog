b19ec48633f153b0cf0ca0bd40d985b746351d47
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-02 10:56:24 +0100

    Config routes, drop db, create new db

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+  root to: 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
-
-  
-
-  root to: 'users#index'
 end
