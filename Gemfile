8535282faac2cc87d0ec7b9297265bc19bbe9d2e
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-03 10:40:28 +0100

    Configure development.rb, and install cancan gem

--- Gemfile
+++ Gemfile
@@ -38,7 +38,8 @@ gem 'letter_opener'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
-gem 'cancancan'
+gem 'cancancan', '~> 1.9'
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :set_global_variables
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 
-  def route_not_found
-    render file: 'public/404.html', status: :not_found, layout: false
+  def set_global_variables
+    @current_user = current_user
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
   end
 end
